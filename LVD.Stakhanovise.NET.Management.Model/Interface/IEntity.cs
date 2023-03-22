efa07d3d99db934cbbfeb8d9cfe31251871d14aa
Alexandru Boia <A.Boia@printecgroup.com>
2023-03-22 16:21:29 +0200

    Initial work at model interface

---
+++ LVD.Stakhanovise.NET.Management.Model/Interface/IEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LVD.Stakhanovise.NET.Management.Model.Interface
+{
+	public interface IEntity<TId, T> : IEquatable<T> where T : class, IEntity<TId, T>
+	{
+		bool IsTransient
+		{
+			get;
+		}
+
+		TId Id
+		{
+			get; set;
+		}
+	}
+}
---
+++ LVD.Stakhanovise.NET.Management.Model/Interface/IModelComplexType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LVD.Stakhanovise.NET.Management.Model.Interface
+{
+	public interface IModelComplexType<T> : IEquatable<T>
+	{
+		T Copy();
+	}
+}
