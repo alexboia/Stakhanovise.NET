1f534242fe91137fd6a182c80ac9f403e5c5a839
Alexandru Boia <A.Boia@printecgroup.com>
2023-03-22 16:07:13 +0200

    Updated management interface tfm and project properties

---
+++ LVD.Stakhanovise.NET.Management.Model/StakhanoviseInstance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LVD.Stakhanovise.NET.Management.Model
+{
+	public class StakhanoviseInstance
+	{
+		public Guid Id
+		{
+			get; set;
+		}
+
+		public string Name
+		{
+			get; set;
+		}
+
+		public string ConnectionString
+		{
+			get; set;
+		}
+
+		public StakhanoviseInstanceProperties Properties
+		{
+			get; set;
+		}
+	}
+}
---
+++ LVD.Stakhanovise.NET.Management.Model/StakhanoviseInstanceProperties.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LVD.Stakhanovise.NET.Management.Model
+{
+	public class StakhanoviseInstanceProperties
+	{
+		public string QueueTableName
+		{
+			get; set;
+		}
+
+		public string ResultsQueueTableName
+		{
+			get; set;
+		}
+
+		public string NewTaskNotificationChannelName
+		{
+			get; set;
+		}
+
+		public string ExecutionTimeStatsTableName
+		{
+			get; set;
+		}
+
+		public string MetricsTableName
+		{
+			get; set;
+		}
+
+		public string DequeueFunctionName
+		{
+			get; set;
+		}
+	}
+}
